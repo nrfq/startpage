c0d60ff400027e23bd8d20fb27f52f2de4521f98
nrfq <43225935+nrfq@users.noreply.github.com>
2021-10-27 14:42:46 -0400

    autofocus search

--- public/js/search.js
+++ public/js/search.js
@@ -5,4 +5,10 @@ $( document ).ready(function() {
       let searchTerm = $("#searchterm").val().replace(" ", "+");
       window.location.replace(`https://www.google.com/search?q=${searchTerm}`);
     });
+    document.addEventListener('keydown', function(event) {
+      let input = document.getElementById("searchterm");
+      if(document.activeElement !== input){
+        input.focus();
+    }
+  });
 });

21d34ff9395ea95b94cc754ef98bd0f1c15777c2
nrfq <43225935+nrfq@users.noreply.github.com>
2021-10-28 14:31:21 -0400

    searchbar handles addresses

--- public/js/search.js
+++ public/js/search.js
@@ -1,9 +1,32 @@
+function testUrl(string) {
+  var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+    '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+    '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+    '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+    '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+    '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+  if(!pattern.test(string))
+    return "";
+  let protocalIndex = string.indexOf('://');
+  if (protocalIndex === -1) {
+      string = `http://${string}`;    
+      return string;
+  };
+}
+
 $( document ).ready(function() {
     $( "#searchterm" ).focus();
     $("#search").submit(function(e){
       e.preventDefault();
-      let searchTerm = $("#searchterm").val().replace(" ", "+");
-      window.location.replace(`https://www.google.com/search?q=${searchTerm}`);
+      let searchTerm = $("#searchterm").val();
+      let url = testUrl(searchTerm);
+      if(url !== ""){
+        window.location.replace(url);
+      }
+      else{
+        searchTerm = searchTerm.replace(" ", "+");
+        window.location.replace(`https://www.google.com/search?q=${searchTerm}`);
+      }
     });
     document.addEventListener('keypress', function(event) {
       let input = document.getElementById("searchterm");
